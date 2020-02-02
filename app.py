29149852c1d6e7bc2fff9dc69a8666e20cf65c47
Abicharan Bhaskaran <abicharanb@pop-os.localdomain>
2020-02-02 11:30:04 +0800

    Minor changes

--- app.py
+++ app.py
@@ -1,5 +1,16 @@
 from flask import Flask, jsonify, request 
 from textgenrnn import textgenrnn
+from nltk.stem.porter import *
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+from sklearn.svm import LinearSVC
+from nltk.stem import SnowballStemmer
+from nltk.stem import WordNetLemmatizer
+from sklearn.feature_extraction.text import TfidfVectorizer
+import string
+import pandas as pd
+import numpy as np
+
 import json
 
 app = Flask(__name__)
@@ -27,13 +38,7 @@ def reviewGen():
 	productID = json.loads(request.data)["id"]
 	
 	#checks if the section is in a valid category
-	category = str(section).lower()
-	if category == "steam":
-		weights = "./models/steam/" + productID + ".hdf5"
-	elif category == "yelp":
-		weights = "./models/yelp/" + productID + ".hdf5"
-	else:
-		return jsonify("Bad request, please enter a valid section")
+	weights = file_search(section,productID)
 	
 	#generates text
 	textgen = textgenrnn(weights)	
@@ -41,13 +46,39 @@ def reviewGen():
 
 	return jsonify(review = review[0], fileUsed = weights)
 
+
+#Gives the negative and positive aspects about a product 
+@app.route("/featureYelp", methods=['GET'])
+def features_extraction():
+	#review_type = json.loads(request.data)["type"]
+	#businessID = json.loads(request.data)["id"]
+	pass
+	
+
 #generates paragraph based on word given
 @app.route("/contextGen", methods=['GET'])
 def contextGen():
+	section = json.loads(request.data)["section"]
+	productID = json.loads(request.data)["id"]
 	keyword = json.loads(request.data)["keyword"]
-	textgen = textgenrnn()
+	weights = file_search(section,productID)
+
+
+	textgen = textgenrnn(weights)
 	output = textgen.generate(return_as_list=True, prefix = keyword)
 	return jsonify(str(output[0]))
+
+
+#search for files
+def file_search(section, productID):
+	category = str(section).lower()
+	if category == "steam":
+		weights = "./models/steam/" + productID + ".hdf5"
+	elif category == "yelp":
+		weights = "./models/yelp/" + productID + ".hdf5"
+	
+	return weights
+
 
 if __name__ == "__main__":
 	app.run(host='0.0.0.0', port=8000)
